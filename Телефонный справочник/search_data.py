e8361e8e260ff49b1d403d54692d65852e44db9d
Lihoradit <kacheli-qt@mail.ru>
2023-02-25 02:00:26 +0300

    Программа для работы с учениками школы

--- Телефонный справочник/search_data.py
+++ Телефонный справочник/search_data.py
@@ -1,21 +1,10 @@
-def export_data():
-    with open('phone.csv', 'r') as file:
-        data = []
-        t = []
-        for line in file:
-            if ',' in line:
-                temp = line.strip().split(',')
-                data.append(temp)
-            elif ';' in line:
-                temp = line.strip().split(';')
-                data.append(temp)
-            elif ':' in line:
-                temp = line.strip().split(':')
-                data.append(temp)        
-            elif line != '':
-                if line != '\n':
-                    t.append(line.strip())
-                else:
-                    data.append(t)
-                    t= []
-    return data
+from export_data import export_data
+from print_data import print_data
+
+def search_data(word, data):
+    if len(data) > 0:
+        for item in data:
+            if word in item:
+                return item
+    else:
+        return None
